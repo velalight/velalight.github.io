c80437a61018c0c38c34e56988749b5af226a5ab
velalight <velalight@aol.com>
2026-08-09 11:52:15 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -9,14 +9,12 @@ self.addEventListener("activate", (event) => {
     (async () => {
       try {
         const keys = await caches.keys();
-
         await Promise.all(
           keys.map((key) => caches.delete(key))
         );
       } catch (e) {
         console.warn("Service worker cache cleanup error:", e);
       }
-
       await self.clients.claim();
     })()
   );
