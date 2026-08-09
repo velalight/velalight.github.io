6180e9acd2c23d891fd636578e3c0f1e70127fe0
velalight <velalight@aol.com>
2026-08-09 10:49:45 +0300

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-// Service worker بسيط - يمنع ظهور 404 في الكونسول
-// ممكن تضيفي عليه كاش للصور والملفات لاحقًا لو عاوزة الموقع يشتغل أوفلاين
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', (e) => e.waitUntil(self.clients.claim()));
