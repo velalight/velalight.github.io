4cd435c770b8e7d91e1caa735e9f772294e8d835
velalight <velalight@aol.com>
2026-08-26 16:41:00 +0300

    Add condition for '.vl-simple-cats' in event listener

--- app.js
+++ app.js
@@ -2527,17 +2527,20 @@ function initNav(){
     });
   });
   $$("[data-cat]").forEach(a=>{
-    if(a.closest(".mnav")||a.closest(".mainnav")||a.closest("footer")){
+    if(a.closest(".mnav")||a.closest(".mainnav")||a.closest("footer")||a.closest(".vl-simple-cats")){
       a.addEventListener("click",()=>{
         setTimeout(()=>{
           const chip=$(`#chips .chip[data-cat="${a.dataset.cat}"]`);
           if(chip){chip.click();}
+          const productsSection = document.getElementById("products");
+          if (productsSection) {
+            productsSection.scrollIntoView({ behavior: "smooth" });
+          }
         },100);
       });
     }
   });
-}
-
+  
 function openDrawer(id,ovlId){
   $("#"+id)?.classList.add("open");
   if(ovlId){$("#"+ovlId)?.classList.add("open");}
